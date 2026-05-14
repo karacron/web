0d516fed386f5ab3d1e6bad22528c7c96b2d7260
sergio.gonzalez <sgonzalez@authuser.org>
2026-05-14 10:35:02 +0200

    Integra GTM en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { ModalProvider } from "@molecule/modal/modal-provider";
+import { PageViewTracker } from "@organism/analytics/page-view-tracker";
 import { CookieConsentBanner } from "@organism/cookies/cookie-consent";
 import SmoothScrollProvider from "@provider/SmoothScrollProvider";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "vanilla-cookieconsent/dist/cookieconsent.css";
 import "./globals.css";
 
@@ -31,6 +33,7 @@ export default async function RootLayout({
 }>) {
   const locale = await getLocale();
   const messages = await getMessages();
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html
@@ -38,10 +41,38 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
       <body className="min-h-full bg-background text-foreground">
+        {gtmId ? (
+          <>
+            <Script id="gtm-consent-default" strategy="beforeInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                window.gtag = window.gtag || function(){window.dataLayer.push(arguments);};
+                window.__karaAnalyticsConsentGranted = false;
+                window.gtag('consent', 'default', {
+                  analytics_storage: 'denied',
+                  ad_storage: 'denied',
+                  ad_user_data: 'denied',
+                  ad_personalization: 'denied',
+                  wait_for_update: 500
+                });
+              `}
+            </Script>
+            <Script id="gtm-base" strategy="beforeInteractive">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');
+              `}
+            </Script>
+          </>
+        ) : null}
         <NextIntlClientProvider messages={messages}>
           <SmoothScrollProvider>
             <ModalProvider>{children}</ModalProvider>
           </SmoothScrollProvider>
+          <PageViewTracker />
           <CookieConsentBanner />
         </NextIntlClientProvider>
       </body>
