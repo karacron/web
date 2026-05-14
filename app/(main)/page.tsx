04318b4d1145ee97076f272bb3ae2d2a85afaa8f
sergio.gonzalez <sgonzalez@authuser.org>
2026-05-14 10:37:42 +0200

    Actualiza home principal

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,17 @@
-import { CtaSection } from '@organism/sections/cta';
-import { FeatureContent } from '@organism/sections/feature/content';
-import { FeatureGrid } from '@organism/sections/feature/grid';
-import { FeatureProduct } from '@organism/sections/feature/product';
-import { HeroSection } from '@organism/sections/hero';
+import { CtaSection } from "@organism/sections/cta";
+import { FeatureGrid } from "@organism/sections/feature/grid";
+import { FeatureProduct } from "@organism/sections/feature/product";
+import { FunctionGrid } from "@organism/sections/function/grid";
+import { HeroSection } from "@organism/sections/hero";
 
 export default function Home() {
-	return (
-		<>
-			<HeroSection />
-			<FeatureProduct />
-			<FeatureGrid />
-			<FeatureContent />
-			<CtaSection />
-		</>
-	);
+  return (
+    <>
+      <HeroSection />
+      <FeatureProduct />
+      <FeatureGrid />
+      <CtaSection />
+      <FunctionGrid />
+    </>
+  );
 }
